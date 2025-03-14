67e1aad24c7187e60dfc3277973e0ade61f83ce2
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2025-03-14 09:48:46 +0900

    update
    Signed-off-by: Yu Ishikawa <yu-iskw@users.noreply.github.com>

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -12,11 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import unittest
 
 
 class TestDummy(unittest.TestCase):
+    """Test dummy."""
 
     def test_dummy(self):
+        """Test dummy."""
         self.assertEqual(1, 2)
